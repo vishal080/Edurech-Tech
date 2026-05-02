95557195aca1700ec995bc340279feb5fe21d959
Goutam Choudhary <rc.goutam.choudhary@gmail.com>
2026-05-02 09:38:00 +0530

    fix netlify routing

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,10 @@ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./db/db.js";
 
+// Start the server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
 // Connect to MongoDB
-connectDB()
-    .then(() => {
-        // Start the server
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log("mongoDB error : ", error);
-    })
+connectDB();
